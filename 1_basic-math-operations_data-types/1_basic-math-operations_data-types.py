089473c61526f667de7f34d9edac27c1269558b0
Ima <278026638+imanurs@users.noreply.github.com>
2026-07-12 19:14:53 +0700

    basic math operations & data types

---
+++ 1_basic-math-operations_data-types/1_basic-math-operations_data-types.py
@@ -0,0 +1,26 @@
+# %%
+# Excercise Day 1
+# Check the python version you are using (Python 3.14.4)
+
+# Open the python interactive shell and do the following operations. The operands are 3 and 4
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**) --> ini pangkat, alias 3 pangkat 4. jadi 3x3x3x3 = 81
+print(3 % 4)             # modulus(%)  --> ini nyari modulo, alias sisa pembagian
+print(3 // 4)            # floor division operator(//) ---> ini nyari pembagian trus dibuletin ke bilangan bulat terdekat
+
+# Write strings on the python interactive shell. The strings are the following
+print('Ima')             #name
+print('Indonesia')       #country
+print('I am really enjoying learning Python')
+
+# Check the data types of the following data:
+print(type(10))          #int - integer. bilangan
+print(type(9.8))         #float - decimal ato pecahan
+print(type(3.14))        #float - decimal ato pecahan
+print(type(4 - 4j))      #complex - campuran bilangan bulat & imajiner
+print(type(['Ima', 'Python', 'Indonesia']))     #list - isinya multiple items
+print(type('Ima'))       #str - string. huruf
+# %%
