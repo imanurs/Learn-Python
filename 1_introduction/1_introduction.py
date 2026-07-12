5138b5b1421a882a2016b6be8e4b7ff370fc84e4
Ima <278026638+imanurs@users.noreply.github.com>
2026-07-12 18:38:04 +0700

    update comment

--- 1_introduction/1_introduction.py
+++ 1_introduction/1_introduction.py
@@ -7,14 +7,14 @@ print(3 + 4)             # addition(+)
 print(3 - 4)             # subtraction(-)
 print(3 * 4)             # multiplication(*)
 print(3 / 4)             # division(/)
-print(3 ** 4)            # exponential(**) ---> ini pangkat, alias 3 pangkat 4. jadi 3x3x3x3 = 81
-print(3 % 4)             # modulus(%)  ---> ini nyari modulo, alias sisa pembagian.  
-print(3 // 4)            # Floor division operator(//)
+print(3 ** 4)            # exponential(**) --> ini pangkat, alias 3 pangkat 4. jadi 3x3x3x3 = 81
+print(3 % 4)             # modulus(%)  --> ini nyari modulo, alias sisa pembagian
+print(3 // 4)            # floor division operator(//) ---> ini nyari pembagian trus dibuletin ke bilangan bulat terdekat
 
 # Write strings on the python interactive shell. The strings are the following
-print('Ima')            # name
-print('Indonesia')      # country
-print('I am enjoying 30 days of python')
+print('Ima')             #name
+print('Indonesia')       #country
+print('I am really enjoying learning Python.')
 
 # Check the data types of the following data:
 print(type(10))
@@ -23,3 +23,4 @@ print(type(3.14))
 print(type(4 - 4j))
 print(type(['Ima', 'Python', 'Indonesia']))
 print(type('Ima'))
+# %%
