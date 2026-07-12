0f92b286e03306d295fe3a445caf5877869f9e15
Ima <278026638+imanurs@users.noreply.github.com>
2026-07-12 15:55:47 +0700

    day 1: basic math operation, data types

---
+++ 1_introduction/day1_introduction.py
@@ -0,0 +1,25 @@
+# %%
+# Excercise Day 1
+# Check the python version you are using (Python 3.14.4)
+
+# Open the python interactive shell and do the following operations. The operands are 3 and 4
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+# Write strings on the python interactive shell. The strings are the following
+print('Ima')            # name
+print('Indonesia')      # country
+print('I am enjoying 30 days of python')
+
+# Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Ima', 'Python', 'Indonesia']))
+print(type('Ima'))
