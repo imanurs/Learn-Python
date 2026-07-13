2c6d9e1e2aa39481037228b755d2e102d73d58a4
Ima <278026638+imanurs@users.noreply.github.com>
2026-07-13 10:33:53 +0700

    update exercise 2 & 3

--- 1_basic-math-operations_data-types/1_basic-math-operations_data-types.py
+++ 1_basic-math-operations_data-types/1_basic-math-operations_data-types.py
@@ -1,5 +1,5 @@
 # %%
-# Excercise Day 1
+# Excercise 1
 # Check the python version you are using (Python 3.14.4)
 
 # Open the python interactive shell and do the following operations. The operands are 3 and 4
@@ -16,11 +16,20 @@ print('Ima')             #name
 print('Indonesia')       #country
 print('I am really enjoying learning Python')
 
+# Exercise 2
+print('\n Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.\n')
 # Check the data types of the following data:
-print(type(10))          #int - integer. bilangan
-print(type(9.8))         #float - decimal ato pecahan
-print(type(3.14))        #float - decimal ato pecahan
-print(type(4 - 4j))      #complex - campuran bilangan bulat & imajiner
+print(type(10))                                 #int - integer. bilangan
+print(type(9.8))                                #float - decimal ato pecahan
+print(type(3.14))                               #float - decimal ato pecahan
+print(type(4 - 4j))                             #complex - campuran bilangan bulat & imajiner
 print(type(['Ima', 'Python', 'Indonesia']))     #list - isinya multiple items
-print(type('Ima'))       #str - string. huruf
-# %%
+print(type('Ima'))                              #str - string. huruf
+print(type(True))                               #bool - boolean. true atau false aja
+print(type({'name':'Ima', 'hobby':'Drawing'}))  #dict - dictionary
+print(type(('Earth', 'Jupiter', 'Neptune')))    #tuple - kaya list tapi ngga bisa diubah. cocok buat data yg udah pakem/fixed
+print(type({1.32, 4, 2.54, 3.14}))              #set - collection of data kaya dict sama list, tapi ini ngga berurutan
+print(type(zip([1,2],[3,4])))                   #zip - built in buat ngegabungin bbrp list jadi pasangan. pasangannya berdasarkan urutan index
+
+# Exercise 2
+print('\n Find an Euclidean distance between (2, 3) and (10, 8)')
